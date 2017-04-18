e53f726ad5b6fc5e7236bbff7dc26e9869a3783b
ufologist <klsforever@gmail.com>
2017-04-18 15:26:29 +0800

    可以在视图层增加 helper 方法用于调试

--- fe/server.js
+++ fe/server.js
@@ -5,6 +5,14 @@ var Mock = require('mockjs');
 var backendTplServer = new BackendTplServer('../src/main/webapp');
 backendTplServer.start();
 
+// 可以在视图层增加 helper 方法用于调试
+// <textarea>$!{__helper.toJSON($var)}</textarea>
+var __helper = {
+    toJSON: function(value) {
+        return JSON.stringify(value, null, 4);
+    }
+};
+
 // 快速定义一个模版页面路由并给他灌入所需的数据
 // 指定要查看哪个页面(这里的 html 即对应后端的模版页面文件)
 // 对应访问的 URL: http://localhost:8000/a/b/c/page.html
@@ -15,12 +23,14 @@ backendTplServer.url('a/b/c/page.html', {
 
 // 如果是想控制更多, 可以直接使用 app(express的实例) 来定义路由, 控制视图和数据
 backendTplServer.app.get('/abc', function(request, response) {
-    var mockData = Mock.mock({
+    var data = Mock.mock({
         id: '@id',
         name: '@cname'
     });
 
-    response.render('abc.html', mockData);
+    data.__helper = __helper;
+
+    response.render('abc.html', data);
 });
 
 // JavaBean 模式
